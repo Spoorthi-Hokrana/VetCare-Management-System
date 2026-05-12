ae78e84e1934c4b0c412198777741a17fc40a9b6
Spoorthi-Hokrana <hokranaspoorthi4@gmail.com>
2026-05-12 14:34:06 +0530

    Fix Vercel read-only filesystem crash

--- config.py
+++ config.py
@@ -6,8 +6,15 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-this-vet-app-secret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'instance', 'vetclinic.db')
+    
+    if os.environ.get('VERCEL') == '1':
+        # Vercel's filesystem is read-only except for /tmp
+        SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/vetclinic.db'
+        UPLOAD_FOLDER = '/tmp/uploads'
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'instance', 'vetclinic.db')
+        UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'uploads')
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Mail Config (Dummy for now)
@@ -17,5 +24,4 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     
-    UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024 # 16 MB max upload
